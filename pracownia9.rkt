6f868d3cca460c8b123a3aad6155266213d3d453
kjpolak <33529557+kjpolak@users.noreply.github.com>
2018-04-24 20:20:28 +0200

    Create pracownia9.rkt

---
+++ pracownia9.rkt
@@ -0,0 +1,227 @@
+#lang racket
+
+;; pomocnicza funkcja dla list tagowanych o określonej długości
+
+(define (tagged-tuple? tag len p)
+  (and (list? p)
+       (= (length p) len)
+       (eq? (car p) tag)))
+
+(define (tagged-list? tag p)
+  (and (pair? p)
+       (eq? (car p) tag)
+       (list? (cdr p))))
+;;
+
+(define (node l r)
+  (list 'node l r))
+
+(define (node? n)
+  (tagged-tuple? 'node 3 n))
+
+(define (node-left n)
+  (second n))
+
+(define (node-right n)
+  (third n))
+
+(define (leaf? n)
+  (or (symbol? n)
+      (number? n)
+      (null? n)))
+
+;;
+
+(define (res v s)
+  (cons v s))
+
+(define (res-val r)
+  (car r))
+
+(define (res-state r)
+  (cdr r))
+
+;;
+
+(define (rename t)
+  (define (rename-st t i)
+    (cond [(leaf? t) (res i (+ i 1))]
+          [(node? t)
+           (let* ([rl (rename-st (node-left t) i)]
+                  [rr (rename-st (node-right t) (res-state rl))])
+             (res (node (res-val rl) (res-val rr))
+                  (res-state rr)))]))
+  (res-val (rename-st t 0)))
+
+;;
+
+(define (st-app f x y)
+  (lambda (i)
+    (let* ([rx (x i)]
+           [ry (y (res-state rx))])
+      (res (f (res-val rx) (res-val ry))
+           (res-state ry)))))
+
+(define get-st
+  (lambda (i)
+    (res i i)))
+
+(define (modify-st f)
+  (lambda (i)
+    (res null (f i))))
+
+;;
+
+(define (inc n)
+  (+ n 1))
+
+(define (rename2 t)
+  (define (rename-st t)
+    (cond [(leaf? t)
+           (st-app (lambda (x y) x)
+                   get-st
+                   (modify-st inc))]
+          [(node? t)
+           (st-app node
+                   (rename-st (node-left  t))
+                   (rename-st (node-right t)))]))
+  (res-val ((rename-st t) 0)))
+
+
+;;
+;; WHILE
+;;
+
+; memory
+
+(define empty-mem
+  null)
+
+(define (set-mem x v m)
+  (cond [(null? m)
+         (list (cons x v))]
+        [(eq? x (caar m))
+         (cons (cons x v) (cdr m))]
+        [else
+         (cons (car m) (set-mem x v (cdr m)))]))
+
+(define (get-mem x m)
+  (cond [(null? m) 0]
+        [(eq? x (caar m)) (cdar m)]
+        [else (get-mem x (cdr m))]))
+
+; arith and bools
+
+(define (const? t)
+  (number? t))
+
+(define (op? t)
+  (and (list? t)
+       (member (car t) '(+ - * / = > >= < <=))))
+
+(define (op-op e)
+  (car e))
+
+(define (op-args e)
+  (cdr e))
+
+(define (op->proc op)
+  (cond [(eq? op '+) +]
+        [(eq? op '*) *]
+        [(eq? op '-) -]
+        [(eq? op '/) /]
+        [(eq? op '=) =]
+        [(eq? op '>) >]
+        [(eq? op '>=) >=]
+        [(eq? op '<)  <]
+        [(eq? op '<=) <=]))
+
+(define (var? t)
+  (symbol? t))
+
+(define (eval-arith e m)
+  (cond [(var? e) (get-mem e m)]
+        [(op? e)
+         (apply
+          (op->proc (op-op e))
+          (map (lambda (x) (eval-arith x m))
+               (op-args e)))]
+        [(const? e) e]))
+
+;;
+
+(define (assign? t)
+  (and (list? t)
+       (= (length t) 3)
+       (eq? (second t) ':=)))
+
+(define (assign-var e)
+  (first e))
+
+(define (assign-expr e)
+  (third e))
+
+(define (if? t)
+  (tagged-tuple? 'if 4 t))
+
+(define (if-cond e)
+  (second e))
+
+(define (if-then e)
+  (third e))
+
+(define (if-else e)
+  (fourth e))
+
+(define (while? t)
+  (tagged-tuple? 'while 3 t))
+
+(define (while-cond t)
+  (second t))
+
+(define (while-expr t)
+  (third t))
+
+(define (block? t)
+  (list? t))
+
+;;
+
+(define (eval e m)
+  (cond [(assign? e)
+         (set-mem
+          (assign-var e)
+          (eval-arith (assign-expr e) m)
+          m)]
+        [(if? e)
+         (if (eval-arith (if-cond e) m)
+             (eval (if-then e) m)
+             (eval (if-else e) m))]
+        [(while? e)
+         (if (eval-arith (while-cond e) m)
+             (eval e (eval (while-expr e) m))
+             m)]
+        [(block? e)
+         (if (null? e)
+             m
+             (eval (cdr e) (eval (car e) m)))]))
+
+(define (run e)
+  (eval e empty-mem))
+
+;;
+
+(define fact10
+  '( (i := 10)
+     (r := 1)
+     (while (> i 0)
+       ( (r := (* i r))
+         (i := (- i 1)) ))))
+
+(define (computeFact10)
+  (run fact10))
+        
+        (define (rand max)
+  (lambda (i)
+    (let ([v (modulo (+ (* 1103515245 i) 12345) (expt 2 32))])
+      (res (modulo v max) v))))
