bdd66961f49adaf6c7d1b61e0f60720396eb0be5
kjpolak <33529557+kjpolak@users.noreply.github.com>
2018-04-27 10:56:57 +0200

    Update prac5.rkt

--- prac5.rkt
+++ prac5.rkt
@@ -53,178 +53,283 @@
 (define (disj-rght formuła)
   (third formuła))
 
-;;;Ćwiczenie2;;;
+#!/usr/bin/env racket
+#lang racket
 
-(define (free-vars formuła)
-  (define (helper acc)
-    (if (null? formuła)
-        acc
-        (if (var? (car formuła))            ;;;and (not (już_zapisana? formuła acc)))
-            (cons formuła acc)
-            (cond [(neg? formuła) (free-vars (neg-subf formuła))]
-                  [(conj? formuła) (cons (free-vars (conj-left formuła)) (free-vars (conj-rght formuła)))];cons nie bardzo tu pasuje
-                  [(disj? formuła) (cons (free-vars (disj-left formuła)) (free-vars (disj-rght formuła)))]))))
-  (helper '()))
+(define (var? t)
+  (symbol? t))
 
+(define (neg? t)
+  (and (list? t)
+       (= 2 (length t))
+       (eq? 'neg (car t))))
 
-(define (free-vars1 f)
-  (cond ((var? f) f)
-        ((neg? f) (free-vars1 (neg-subf f)))
-        ((conj? f) (cons (free-vars1 (conj-left f))
-                         (free-vars1 (conj-rght f))))
-        ((disj? f) (cons (free-vars1 (disj-left f))
-                         (free-vars1 (disj-rght f))))))
+(define (conj? t)
+  (and (list? t)
+       (= 3 (length t))
+       (eq? 'conj (car t))))
 
-(define (już_zapisana? zmienna lista)
-  (cond ([(null? lista) '(#f)]
-         [(eq? zmienna (car lista)) #t]
-         [(już_zapisana? zmienna (cdr lista))])))
+(define (disj? t)
+  (and (list? t)
+       (= 3 (length t))
+       (eq? 'disj (car t))))
 
-;(free-vars1 (list 'conj (list 'disj 'p 'q) 'q))
+(define (prop? f)
+  (or (var? f)
+      (and (neg? f)
+           (prop? (neg-subf f)))
+      (and (disj? f)
+           (prop? (disj-left f))
+           (prop? (disj-rght f)))
+      (and (conj? f)
+           (prop? (conj-left f))
+           (prop? (conj-rght f)))))
+
+;;; Zadanie 1
+
+(define (neg s)
+  (list 'neg s))
+
+(define (neg-subf t)
+  (second t))
+
+(define (conj sl sr)
+  (list 'conj sl sr))
+
+(define (conj-left t)
+  (second t))
+
+(define (conj-rght t)
+  (third t))
+
+(define (disj sl sr)
+  (list 'disj sl sr))
+
+(define (disj-left t)
+  (second t))
+
+(define (disj-rght t)
+  (third t))
+
+;;; Zadanie 2
+
+(define (free-vars t)
+  (define (aux t fv)
+    (cond [(var? t) (cons t fv)]
+          [(neg? t) (aux (neg-subf t) fv)]
+          [(disj? t) (aux (disj-left t) (aux (disj-rght t) fv))]
+          [(conj? t) (aux (conj-left t) (aux (conj-rght t) fv))]))
+  (remove-duplicates (aux t null)))
+
+;;;;(displayln (free-vars (neg (conj 'a (disj 'b 'a)))))
 
 (define (gen-vals xs)
-  (if ( null? xs )
-    ( list null )
-    ( let*
-         (( vss ( gen-vals ( cdr xs ) ) )
-         ( x ( car xs ) )
-         ( vst ( map ( lambda ( vs ) ( cons ( list x true ) vs ) ) vss ) )
-         ( vsf ( map ( lambda ( vs ) ( cons ( list x false ) vs ) ) vss ) ) )
-      ( append vst vsf ) ) ) )
-;(gen-vals (list 'p 'q))
+  (if (null? xs)
+      (list null)
+      (let* ((vss (gen-vals (cdr xs)))
+             (x (car xs))
+             (vst (map (lambda (vs) (cons (list x true) vs)) vss))
+             (vsf (map (lambda (vs) (cons (list x false) vs)) vss)))
+        (append vst vsf))))
 
-(define (eval-formula formula wartosciowanie)
-  (cond ([(var? formula) (bool formula wartosciowanie)]
-         [(conj? formula) (and (eval-formula (conj-left formula) wartosciowanie) (eval-formula (conj-rght formula) wartosciowanie))]
-         [(disj? formula) (and (eval-formula (disj-left formula) wartosciowanie) (eval-formula (disj-rght formula) wartosciowanie))])))
+;;; Zadanie 3
 
-(define (bool zmienna wartościowanie)
-  (cond([(eq? (car (car wartościowanie)) zmienna) (car (cdr wartościowanie))]
-        [(null? wartościowanie) (error "Zmienna nie występuje w podanej liście wartośiowań")]
-        [(< 0 1) (bool zmienna (cdr wartościowanie))])))
-(eval-formula (list 'disj 'p 'q) '((p #t) (q #t)))
+(define (var-val v vals)
+    (cond [(null? vals) (error "Undefined variable" v)]
+          [(eq? v (caar vals)) (cadar vals)]
+          [else (var-val v (cdr vals))]))
 
-(define (free-vars1 f)
-  (cond ((var? f) f)
-        ((neg? f) (free-vars1 (neg-subf f)))
-        ((conj? f) (cons (free-vars1 (conj-left f))
-                         (free-vars1 (conj-rght f))))
-        ((disj? f) (cons (free-vars1 (disj-left f))
-                         (free-vars1 (disj-rght f))))))
+(define (eval-formula t vals)
+  (cond [(var? t)  (var-val t vals)]
+        [(neg? t)  (not (eval-formula (neg-subf t) vals))]
+        [(disj? t) (or  (eval-formula (disj-left t) vals)
+                        (eval-formula (disj-rght t) vals))]
+        [(conj? t) (and (eval-formula (conj-left t) vals)
+                        (eval-formula (conj-rght t) vals))]))
 
-(define (już_zapisana? zmienna lista)
-  (cond ([(null? lista) '(#f)]
-         [(eq? zmienna (car lista)) #t]
-         [(już_zapisana? zmienna (cdr lista))])))
+(define (falsifiable-eval? t)
+  (define all-vals (gen-vals (free-vars t)))
+  (define (aux t vals)
+    (cond [(null? vals) #f]
+          [(eq? #f (eval-formula t (car vals))) (car vals)]
+          [else (aux t (cdr vals))]))
+  (aux t all-vals))
 
-;(free-vars1 (list 'conj (list 'disj 'p 'q) 'q))
-;;;Zadanie3;;;
-(define (gen-vals xs)
-  (if ( null? xs )
-    ( list null )
-    ( let*
-         (( vss ( gen-vals ( cdr xs ) ) )
-         ( x ( car xs ) )
-         ( vst ( map ( lambda ( vs ) ( cons ( list x true ) vs ) ) vss ) )
-         ( vsf ( map ( lambda ( vs ) ( cons ( list x false ) vs ) ) vss ) ) )
-      ( append vst vsf ) ) ) )
-;(gen-vals (list 'p 'q))
+;;; Zadanie 4
 
-(define (eval-formula formula wartosciowanie)
-  (cond ([(var? formula) (bool formula wartosciowanie)]
-         [(neg? formula) (not (eval-formula (neg-subf formula)))]
-         [(conj? formula) (and (eval-formula (conj-left formula) wartosciowanie) (eval-formula (conj-rght formula) wartosciowanie))]
-         [(disj? formula) (and (eval-formula (disj-left formula) wartosciowanie) (eval-formula (disj-rght formula) wartosciowanie))])))
+(define (lit? t)
+  (and (list? t)
+       (= 3 (length t))
+       (eq? 'lit (first t))
+       (or (eq? 'normal (second t))
+           (eq? 'negatv (second t)))
+       (var? (third t))))
 
-#|(define (bool zmienna wartościowanie);;;lub procedura assoc - robi dokładnie prawie to samo
-  (cond([(eq? (car (car wartościowanie)) zmienna) (car (cdr wartościowanie))]
-        [(null? wartościowanie) (error "Zmienna nie występuje w podanej liście wartośiowań")]
-        [(< 0 1) (bool zmienna (cdr wartościowanie))])))
-|#
+(define (lit n v)
+  (list 'lit
+        (if n 'normal 'negatv)
+        v))
 
-(define (bool zmienna wartosciowanie)
-  (if (null? wartosciowanie)
-      (error "error")
-      (if (eq? (car (car wartosciowanie)) zmienna)
-          (cdr (car wartosciowanie))
-          (bool zmienna (cdr wartosciowanie)))))
+(define (lit-neg? t)
+  (eq? (second t) 'negatv))
 
-(define (falsifiable-eval? f)
-  (define (iter vals)
-    (if (null? vals)
-        #f
-        (if (eval-formula f (car vals))
-            (iter (cdr vals))
-            (car vals))))
-  (iter (gen-vals (free-vars f))))
-;(eval-formula (list 'disj 'p 'q) '((p #t) (q #t)))
+(define (lit-subf t)
+  (third t))
 
-;;;Ćwiczenie4;;;
+(define (lit-negate t)
+  (lit (not (lit-neg? t)) (lit-subf t)))
 
-(define (literal p)
-  (if (or (var? p)
-          (and (neg? p)
-               (var? (neg-subf p))))
-      (list 'literal p)
-      (error "brak")))
+(define (nnf? f)
+  (or (var? f) 
+    (and (neg? f) 
+         (var? (neg-subf f)))
+    (lit? f)
+    (and (disj? f) 
+         (nnf? (disj-left f))
+         (nnf? (disj-rght f))) 
+    (and (conj? f) 
+         (nnf? (conj-left f))
+         (nnf? (conj-rght f)))))
 
-(define (literal? l)
-  (and (list? l)
-       (= 2 (length l))
-       (eq? (car l) 'literal)
-       (or (var? (second l))
-           (and (neg? (second l))
-                (var? (neg-subf (second l)))))))
-#|(define (nnf? f)
-  (cond ([(literal? f) #t]
-         [(conj? f) (and (nnf (conj-left f)) (nnf (conj-rght f)))]
-         [(disj? f) (or (nff (disj-left f)) (nnf (disj-rght f)))]
-         [(neg? f) #f]
-         [else (error "Podano nieprawidłowa formule")])))
-|#
-;;;Ćwiczenie5;;;
+(define (free-vars-bis t)
+  (define (aux t fv)
+    (cond [(var? t) (cons t fv)]
+          [(lit? t) (cons (lit-subf t) fv)]
+          [(neg? t) (aux (neg-subf t) fv)]
+          [(disj? t) (aux (disj-left t) (aux (disj-rght t) fv))]
+          [(conj? t) (aux (conj-left t) (aux (conj-rght t) fv))]))
+  (remove-duplicates (aux t null)))
+
+(define (eval-formula-bis t vals)
+  (cond [(var? t)  (var-val t vals)]
+        [(lit? t)  ((if (lit-neg? t) not identity) (var-val (lit-subf t) vals))]
+        [(neg? t)  (not (eval-formula-bis (neg-subf t) vals))]
+        [(disj? t) (or  (eval-formula-bis (disj-left t) vals)
+                        (eval-formula-bis (disj-rght t) vals))]
+        [(conj? t) (and (eval-formula-bis (conj-left t) vals)
+                        (eval-formula-bis (conj-rght t) vals))]))
+
+(define (falsifiable-eval-bis? t)
+  (define all-vals (gen-vals (free-vars-bis t)))
+  (define (aux t vals)
+    (cond [(null? vals) #f]
+          [(eq? #f (eval-formula-bis t (car vals))) (car vals)]
+          [else (aux t (cdr vals))]))
+  (aux t all-vals))
+
+;;; Zadanie 5
 
 (define (convert-to-nnf f)
-  (cond ([(var? f) (literal f)]
-         [(neg? f) (convert-to-nnf-neg (neg-subf f))]
-         [(conj? f) (conj (convert-to-nnf (conj-left)) (convert-to-nnf (conj-rght)))]
-         [(disj? f) (disj (convert-to-nnf (disj-left)) (convert-to-nnf (disj-rght)))])))
+  (define (neg-formula f)
+      (cond [(var? f) (lit #f f)]
+            [(lit? f) (lit-negate f)]
+            [(neg? f) (convert-to-nnf (neg-subf f))]
+            [(disj? f) (conj (neg-formula (disj-left f))
+                             (neg-formula (disj-rght f)))]
+            [(conj? f) (disj (neg-formula (conj-left f))
+                             (neg-formula (conj-rght f)))]))
+  (cond [(var? f) (lit #t f)]
+        [(lit? f) f]
+        [(neg? f) (neg-formula (neg-subf f))]
+        [(disj? f) (disj (convert-to-nnf (disj-left f))
+                         (convert-to-nnf (disj-rght f)))]
+        [(conj? f) (conj (convert-to-nnf (conj-left f))
+                         (convert-to-nnf (conj-rght f)))]))
 
-(define (convert-to-nnf-neg f)
-  (cond ([(var? f) (literal (neg f))]
-         [(disj? f) (conj (convert-to-nnf (disj-left f)) (convert-to-nnf (disj-rght f)))]
-         [(conj? f) (disj (convert-to-nnf (conj-left f)) (convert-to-nnf (conj-rght f)))]
-         [(neg? f) (convert-to-nnf (neg-subf f))])))
-(convert-to-nnf (list 'f))
+;;; (convert-to-nnf (neg (neg (disj (neg (conj 'a 'c)) (neg 'b)))))
 
-;;;Ćwiczenie6;;;
+;;; Zadanie 6
 
-(define (klazula lista_klauzul)
-  (cons 'cl lista_klauzul))
+(define (cnf? l)
+  (define (is-disj? l)
+    (and (list? l)
+         (<= 1 (length l))
+         (eq? 'cnf-disj (car l))
+         (andmap lit? (cdr l))))
+  (and (list? l)
+       (<= 1 (length l))
+       (eq? 'cnf-conj (car l))
+       (andmap is-disj? (cdr l))))
 
-(define (klauzula? f)
-  (and? (eq? (car f) 'cl)
-        (pair? f)
-        (list? (cdr f))
-        (andmap literal? (cdr f))))
+(define (lit->cnf l)
+  (list 'cnf-conj (list 'cnf-disj l)))
 
-(define (cnf? f)
-  (and (pair? f)
-       (eq? (car f) 'cnf)
-       (list? (cdr f))
-       (andmap cl? (cdr f))))
+(define (cnf-conj->cnf c1 c2)
+  (append (list 'cnf-conj) (cdr c1) (cdr c2)))
 
-(define (cnf klauzule)
-  (cons 'cnf klauzule))
+(define (cnf-disj->cnf c1 c2)
+  (define (mmerge xs)
+    (append (list 'cnf-disj) (apply append xs)))
+  (append (list 'cnf-conj) (map mmerge (cartesian-product (map cdr (cdr c1)) (map cdr (cdr c2))))))
 
 (define (convert-to-cnf f)
-  (cond ([(literal? f) (cnf (list (klauzula (list f))))]
-         [(conj? f) (append (cdr (convert-to-cnf (conj-left f))) (cdr (convert-to-cnf (conj-rght f))))];tag cnf?
-         [(disj? f) (cnf (join (cdr (disj-left f)) (cdr (disj-rght f))))])))
+  (cond [(lit? f) (lit->cnf f)]
+        [(conj? f) 
+          (let ((left (convert-to-cnf (conj-left f)))
+                (rght (convert-to-cnf (conj-rght f))))
+            (cnf-conj->cnf left rght))]
+        [(disj? f) 
+          (let ((left (convert-to-cnf (disj-left f)))
+                (rght (convert-to-cnf (disj-rght f))))
+            (cnf-disj->cnf left rght))]))
 
-(define (join xss yys)
-  (apply append(map (lambda (p) (map (lambda (q) (append p (cdr q))yss) xss)))))
+(define (display-cnf cs)
+  (define (display-cnf-disj cs)
+    (define (display-cnf-lit l)
+      (format "~a~a" (if (lit-neg? l) "¬" "") (lit-subf l)))
+    (string-join (map display-cnf-lit (cdr cs)) " | "
+                 #:before-first "(" #:after-last ")"))
+  (string-join (map display-cnf-disj (cdr cs)) " & "
+               #:before-first "(" #:after-last ")"))
+
+;;; (cnf? (list 'cnf-conj))
+;;; (cnf? (list 'cnf-conj (list 'cnf-disj)))
+;;; (cnf? (list 'cnf-conj (list 'cnf-disj (lit #t 'a) (lit #f 'a))))
+
+(define first-expr
+  (cnf-conj->cnf 
+    (lit->cnf (lit #t 'a))
+    (cnf-disj->cnf (lit->cnf (lit #t 'b)) 
+                       (lit->cnf (lit #f 'c)))))
+
+(define first-expr-nnf
+  (conj (lit #t 'a) (disj (lit #t 'b) (lit #f 'c))))
+
+(define secon-expr
+  (cnf-conj->cnf 
+    (lit->cnf (lit #t 'd))
+    (cnf-disj->cnf (lit->cnf (lit #t 'e)) 
+                       (lit->cnf (lit #f 'f)))))
+
+(define secon-expr-nnf
+  (conj (lit #t 'd) (disj (lit #t 'e) (lit #f 'f))))
+
+(define my-expr 
+  (cnf-disj->cnf first-expr secon-expr))
+
+(define my-expr-nnf
+  (disj first-expr-nnf secon-expr-nnf))
+
+;;; (cnf? (convert-to-cnf my-expr-nnf))
 
 
+(define (eval-cnf t vals)
+  (define (eval-cnf-disj t)
+    (define (eval-lit t)
+      ((if (lit-neg? t) not identity) (var-val (lit-subf t) vals)))
+    (ormap eval-lit (cdr t)))
+  (andmap eval-cnf-disj (cdr t)))
 
+(define (free-vars-cnf t)
+  (remove-duplicates (apply append (map (lambda (t) (map (lambda (l) (lit-subf l)) (cdr t))) (cdr t)))))
 
+(define (falsifiable-cnf? t)
+  (define all-vals (gen-vals (free-vars-cnf t)))
+  (define (aux t vals)
+    (cond [(null? vals) #f]
+          [(eq? #f (eval-cnf t (car vals))) (car vals)]
+          [else (aux t (cdr vals))]))
+  (aux t all-vals))
+
+(sort (falsifiable-eval-bis? my-expr-nnf) string<? #:key (lambda (x) (symbol->string (car x))))
+(sort (falsifiable-cnf? (convert-to-cnf my-expr-nnf)) string<? #:key (lambda (x) (symbol->string (car x))))
